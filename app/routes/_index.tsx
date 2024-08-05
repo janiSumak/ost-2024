bfaeda5ce2e1cc32bd8f2e7db773c036f2ef10d7
Jani Šumak <jani.sumak@inova.si>
2024-08-05 09:17:24 +0200

    Updated remix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,48 +1,19 @@
-import type { MetaFunction } from "@remix-run/node";
+import './App.css';
+import Inova from "../../public/inova.svg"
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-export default function Index() {
+function App() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <img src={Inova} alt="Inova IT Logo" className="App-logo" />
+        <h1 className="App-title">INOVA IT</h1>
+        <p className="App-description">
+          We are an exceptional team of engineers, designers and artists who research, design and
+          develop mobile solutions that people love.
+        </p>
+      </header>
     </div>
   );
 }
+
+export default App;
