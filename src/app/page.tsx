41d1ce99d22fef4c216b7383d5c9c941f6a80de3
Jani Šumak <jani.sumak@inova.si>
2024-08-05 09:29:52 +0200

    Added next inova project

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import Inova from "../../public/inova.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Image src={Inova} alt="Inova IT Logo" className="App-logo" />
+        <h1 className="App-title">INOVA IT</h1>
+        <p className="App-description">
+          We are an exceptional team of engineers, designers and artists who research, design and
+          develop mobile solutions that people love.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
